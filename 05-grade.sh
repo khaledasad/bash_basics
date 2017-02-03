3dbacad1db5cfc9470d74a7479462b21860d15fc
ASAD <kasad@ggc.edu>
2017-02-03 11:40:00 -0500

    Khaled Asad 05-Grade

--- 05-grade.sh
+++ 05-grade.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 echo "What did you get in the first ITEC 3860 test?"
 echo "Please give a numeric answer"
 read grade
@@ -14,6 +13,27 @@ elif [ $grade -ge 70 ]; then
 else
    echo "Time to work on some extra credit assignments"
 fi
+
+
+
+
+echo "What temperature is it ?"
+echo "Please give a numeric answer"
+read temp
+
+if [ $temp -ge 70 ]; then
+   echo "It is hot"
+elif [ $temp -ge 60 ]; then
+   echo "It is okay."
+elif [ $temp -ge 40 ]; then
+   echo "It is chilly."
+else
+   echo "It is cold"
+fi
+
+
+
+
 
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40

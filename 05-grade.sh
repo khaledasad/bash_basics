8fde63ea204f7549ce0e4d0cc3ff284b3fd1cb4b
ASAD <kasad@ggc.edu>
2017-02-03 11:51:17 -0500

    Khaled Asad 05-Grade NEWUPDATE

--- 05-grade.sh
+++ 05-grade.sh
@@ -15,6 +15,23 @@ else
    echo "Time to work on some extra credit assignments"
 fi
 
+
+echo "Please Pick your Temperature that you would like?"
+echo "Make sure its NUMERIC input"
+read temp
+
+if [ $temp -ge 70 ]; then
+   echo "It is really hot."
+elif [ $temp -ge 60 ]; then
+   echo "It is Okey i guess."
+elif [ $temp -ge 40 ]; then
+   echo "It is kinda Chilly."
+else
+   echo "Ok it is really cold!"
+fi
+
+
+
 # exercise: write a script (that utilizes weather-util) 
 # that prints "it's cold" if the temperature is < 40
 # it's chilly if < 60, it's okay if < 70 and, it's hot for 
